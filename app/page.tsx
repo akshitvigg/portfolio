156d9a5475b3a51c1a7ed7cc23e365043d08b72a
Akshit Vig <akshitvig48@gmail.com>
2024-12-23 00:01:40 +0530

    skills

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,38 @@
+import { IconCloud } from "@/components/skillssphere";
 import Image from "next/image";
+
+const slugs = [
+  "typescript",
+  "javascript",
+  "dart",
+  "java",
+  "react",
+  "flutter",
+  "android",
+  "html5",
+  "css3",
+  "nodedotjs",
+  "express",
+  "nextdotjs",
+  "prisma",
+  "amazonaws",
+  "postgresql",
+  "firebase",
+  "nginx",
+  "vercel",
+  "testinglibrary",
+  "jest",
+  "cypress",
+  "docker",
+  "git",
+  "jira",
+  "github",
+  "gitlab",
+  "visualstudiocode",
+  "androidstudio",
+  "sonarqube",
+  "figma",
+];
 
 export default function Home() {
   return (
@@ -9,7 +43,7 @@ export default function Home() {
           <p>Full Stack developer </p>
         </div>
         <Image
-          className=" ml-32 rounded-full"
+          className=" ml-52 rounded-full"
           src={
             "https://i.pinimg.com/736x/aa/c8/02/aac8024b2258aad7262895c4b0bccd79.jpg"
           }
@@ -18,8 +52,22 @@ export default function Home() {
           height={300}
         />
       </div>
-      <p className="pl-[390px] text-xl font-bold ">About</p>
-      <p className="pl-[390px] text-zinc-400">dsd</p>
+      <p className="pl-[360px]  text-xl font-bold ">About</p>
+      <p className="pl-[360px]  w-[1000px] text-zinc-400">
+        I’m Akshit, a self-taught programmer who loves diving into new things
+        and figuring them out as I go. Right now, I’m learning Japanese and
+        exploring cool areas like AI and blockchain. I’m all about building
+        things with code, breaking them down, and making them better. I’ve
+        always had an interest in art, even if I’m not great at it, but these
+        days, I’m focused on creating stuff that actually works and solving
+        problems with technology.
+      </p>
+
+      <div className=" flex justify-center">
+        <div className=" w-96 bg-zinc-600">dsd</div>
+      </div>
+
+      <IconCloud iconSlugs={slugs} />
     </div>
   );
 }
