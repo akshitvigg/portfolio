3503bc677484097e8482a6a61d03236397bc36fc
akshitvigg <akshitvig48@gmail.com>
2024-12-24 02:56:16 +0530

    dark styles done

--- components/projectsCard.tsx
+++ components/projectsCard.tsx
@@ -17,7 +17,7 @@ export const Card = ({
   projLink,
 }: CardProps) => {
   return (
-    <div className=" border  rounded-lg border-zinc-800 h-[365px] w-96">
+    <div className=" border  rounded-lg dark:border-zinc-800 h-[365px] w-96">
       <Link href={projLink}>
         <Image
           className=" rounded-t-lg  "
@@ -28,18 +28,20 @@ export const Card = ({
         />
       </Link>
       <p className=" font-bold text-lg pt-1 pl-2">{projectTitle}</p>
-      <p className="text-sm text-neutral-400 pt-1 pl-2">{description}</p>
+      <p className="text-sm text-neutral-500 dark:text-neutral-400 pt-1 pl-2">
+        {description}
+      </p>
       <div className="mt-2 flex">
         {toolsUsed.map((tools, index) => (
           <div key={index}>
-            <button className="border bg-neutral-800 px-2 text-xs border-zinc-700 rounded-full ml-1 py-1">
+            <button className="border bg-neutral-200 dark:bg-neutral-800 px-2 text-xs border-neutral-400 dark:border-zinc-700 rounded-full ml-1 py-1">
               {tools}
             </button>
           </div>
         ))}
       </div>
       <Link href={projLink}>
-        <button className=" hover:bg-zinc-300 active:scale-105 transition-all duration-150 rounded-md py-1 ml-2 mt-3 flex items-center bg-white text-black text-sm px-2">
+        <button className=" hover:bg-zinc-300 active:scale-105 transition-all duration-150 rounded-md py-1 ml-2 mt-3 flex border-neutral-300 border items-center dark:bg-white dark:text-black text-sm px-2">
           <img src="/network.png" width={12} />
           <p className="pl-2  ">Website</p>
         </button>
