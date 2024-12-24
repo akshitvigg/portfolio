2c5ebe5187da72fa8857c4c67c9f63e88ec73556
Akshit Vig <akshitvig48@gmail.com>
2024-12-24 16:29:49 +0530

    pushed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { Analytics } from "@vercel/analytics/next";
+import { NextUIProvider } from "@nextui-org/system";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider>{children}</ThemeProvider>
+        <NextUIProvider>
+          <ThemeProvider>{children}</ThemeProvider>
+        </NextUIProvider>
         <Analytics />
       </body>
     </html>
