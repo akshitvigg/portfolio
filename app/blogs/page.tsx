ea91e691d22b09bb9d9fe6b4d40f7daf0a3fabf3
Akshit Vig <akshitvig48@gmail.com>
2025-03-16 01:03:50 +0530

    prop drlled

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,6 +1,6 @@
 const Blogs = () => {
   return (
-    <div>
+    <div className="flex justify-center">
       <div>
         <p>My first blog</p>
         <p>i will post my first blog today so be prepared</p>
